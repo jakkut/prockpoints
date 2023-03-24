1efe542408a14723c5700113b33d2cd2939fd766
Jack Thompson <jakkuthompson@gmail.com>
2023-03-24 18:39:29 -0400

    Fixed a render bug, updated Sam haha

--- src/points.js
+++ src/points.js
@@ -5,6 +5,8 @@ import "./wobble.css";
 import * as converter from "number-to-words";
 import Sam from "./sam.jpg";
 
+import WobbleText from "./wobble";
+
 const link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQIlV6a8K6PXlrdHZ80_CzxAgdU3imRsD30mITMG-M7cHKl46i1y5adbZ8ht09rQ2TbBM_fJ8_Q-wky/pub?gid=0&single=true&output=csv";
 
 
@@ -48,30 +50,6 @@ function App() {
             }
         );
     }, []);
-
-    useEffect(() => {
-        // SOME CODE I FOUND AT https://codepen.io/queenadreena/pen/oKGyYq
-        // eslint-disable-next-line
-        {
-            // Create array of any elements with "wobble" class
-            const all = document.querySelectorAll('.wobble');
-
-            // Iterate through each "wobble"
-            all.forEach(el => {
-            // Get the text content of the element
-            let text = el.textContent;
-            // Create an array of separate letters
-            text = text.split("");
-            // Iterate through each letter and give it its own span element and individual animation delay offset
-            const textCode = text.map((x, idx) => {
-                let delay = (idx + 1) * 50;
-                return `<span style="animation-delay: ${delay}ms">${x}</span>`;
-            })
-            // replace the element's html with our dynamically created html
-            el.innerHTML = textCode.join("");
-            });
-        }
-    })
     
     return (
         <div className="App">
@@ -80,7 +58,7 @@ function App() {
                 <div className="content">
                     <div className="header">
                         <h1>Welcome to<br/>
-                            <span className="wobble">ProckPoints.Gay</span>
+                        <WobbleText textToWobble={"ProckPoints.Gay"}/>
                         </h1>
                     </div>
                     <p>
