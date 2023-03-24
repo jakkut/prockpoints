291bb149e984e021fe9f432bcd9bcc1f7b8b76a7
Jack Thompson <jakkuthompson@gmail.com>
2023-03-24 16:34:55 -0400

    Added site title, neat wobble effect

--- src/points.js
+++ src/points.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useMemo } from "react";
 import Papa from "papaparse";
 import "./style.css";
+import "./wobble.css";
 import * as converter from "number-to-words";
 import Sam from "./sam.jpg";
 
@@ -47,39 +48,66 @@ function App() {
             }
         );
     }, []);
+
+    useEffect(() => {
+        // SOME CODE I FOUND AT https://codepen.io/queenadreena/pen/oKGyYq
+        // eslint-disable-next-line
+        {
+            // Create array of any elements with "wobble" class
+            const all = document.querySelectorAll('.wobble');
+
+            // Iterate through each "wobble"
+            all.forEach(el => {
+            // Get the text content of the element
+            let text = el.textContent;
+            // Create an array of separate letters
+            text = text.split("");
+            // Iterate through each letter and give it its own span element and individual animation delay offset
+            const textCode = text.map((x, idx) => {
+                let delay = (idx + 1) * 50;
+                return `<span style="animation-delay: ${delay}ms">${x}</span>`;
+            })
+            // replace the element's html with our dynamically created html
+            el.innerHTML = textCode.join("");
+            });
+        }
+    })
     
     return (
         <div className="App">
-            {Object.keys(CSVData).length > 0 && (
-                <p>
-                There are currently{" "}
-                <b>
-                    {howMany} (
-                    {converter.toWords(howMany)}) Prock
-                    Points&trade;
-                </b>{" "}
-                in circulation.
-                </p>
-            )}
             {!Object.keys(CSVData).length && <p>Loading...</p>}
             {Object.keys(CSVData).length > 0 && (
-                <table>
-                <thead>
-                    <tr>
-                    <th>PEOPLE:</th>
-                    <th>BALANCE:</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(CSVData).map((i) => (
-                    <PointRow
-                        key={i}
-                        name={CSVData[i]["PERSON"]}
-                        num={CSVData[i]["BALANCE"]}
-                    />
-                    ))}
-                </tbody>
-                </table>
+                <div className="content">
+                    <div className="header">
+                        <h1>Welcome to <span className="wobble">ProckPoints.Gay</span></h1>
+                    </div>
+                    <p>
+                    There are currently{" "}
+                    <b>
+                        {howMany} (
+                        {converter.toWords(howMany)}) Prock
+                        Points&trade;
+                    </b>{" "}
+                    in circulation.
+                    </p>
+                    <table>
+                        <thead>
+                            <tr>
+                            <th>PEOPLE:</th>
+                            <th>BALANCE:</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Object.keys(CSVData).map((i) => (
+                            <PointRow
+                                key={i}
+                                name={CSVData[i]["PERSON"]}
+                                num={CSVData[i]["BALANCE"]}
+                            />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             )}
             {samImage && (
                 <div className="sam-div">
